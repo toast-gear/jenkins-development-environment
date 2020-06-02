7151d5223241617551cdddc23bc195559433b10f
toast-gear <15716903+toast-gear@users.noreply.github.com>
2020-06-02 09:44:28 +0100

    feat: adding example to get all jobs

---
+++ docs/groovy/get-jobs.groovy
@@ -0,0 +1,5 @@
+// This will get all jobs recursively whether at the root of Jnkeins, in a folder or an in organisation folder
+
+for(job in Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)) {
+  println(job.fullname)
+}
