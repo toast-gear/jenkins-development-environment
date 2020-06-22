d741884efbba7e7f948f461c9da3cb78a5862aae
toast-gear <15716903+toast-gear@users.noreply.github.com>
2020-06-22 16:22:05 +0100

    doc: adding some details

--- docs/groovy/delete-build-history.groovy
+++ docs/groovy/delete-build-history.groovy
@@ -1,10 +1,23 @@
+#!/usr/bin/env groovy
+
+
+/*
+    Delete all build history outside of defined amount. This is needed as excess build history
+    can result in an unresponsive Jenkins instance. This script is recursive and supports GitHib
+    org scanners as well as folders.
+    
+    =~ is knowm as the find operator and uses regex. To escape / use a \ e.g.
+    /some-folder/some-job/
+
+*/
+
 MAX_BUILDS = 40
 
 def jobsList = []
 for(job in Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)) {
-  if(job.fullName =~ /my-jenkins-job-name/)
+  if(job.fullName =~ /my-jenkins-bob/)
   {
-    //println("Adding job: " + job.fullName)
+    println("Adding job: " + job.fullName)
     jobsList.add(job)
   }
 }
@@ -14,11 +27,8 @@ for (i = 0; i <5; i++) {
     def recent = job.builds.limit(MAX_BUILDS)
     for (build in job.builds) {
       if (!recent.contains(build)) {
-        println ""
-        println "========================================================="
         println "Preparing to delete: " + build
-        //build.delete()
-        println ""
+        // build.delete()
       }
     }
   }
