373bfe26ae4bc6ebb241921bb0d6f8c34898aadf
toast-gear <15716903+toast-gear@users.noreply.github.com>
2020-08-17 13:37:42 +0100

    feat: adding 2nd decrypt credentials method

--- docs/groovy/decrypt-passwords.groovy
+++ docs/groovy/decrypt-passwords.groovy
@@ -3,8 +3,27 @@
 /*
 
   Get the encrypted password from the credentials.xml file on the Jenkins master.
-  Replace {XXX=} with the encrypted value (including the surrounding {})
+  Replace {XXX=} with the encrypted value (including the surrounding {}). Run in
+  https://$JENKINS_URL/script
 
 */
 
 println(hudson.util.Secret.fromString("{XXX=}").getPlainText())
+
+
+/*
+
+  Run the below in https://$JENKINS_URL/script
+
+*/
+
+def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+com.cloudbees.plugins.credentials.common.StandardCredentials.class,
+    Jenkins.instance,
+    null,
+    null
+);for (int i; i < creds.size(); i++) {
+  println "\n========== Credential ${i+1} Start =========="
+  creds[i].properties.each { println it }
+  println "========== Credential ${i+1} End   ==========\n"
+}
