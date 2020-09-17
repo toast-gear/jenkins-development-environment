18ee0b0c23f821306710e2aae32533782c9bc3ce
toast-gear <15716903+toast-gear@users.noreply.github.com>
2020-09-17 11:55:46 +0100

    feat: adding cancel jobs groovy

--- docs/groovy/get-jobs.groovy
+++
@@ -1,5 +0,0 @@
-// This will get all jobs recursively whether at the root of Jnkeins, in a folder or an in organisation folder
-
-for(job in Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)) {
-  println(job.fullName)
-}
